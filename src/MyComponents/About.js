c8fc4be82fcd6bb5bd55a562de6e545da69b066d
Amit Kumar <77743439+amitsarmal@users.noreply.github.com>
2021-08-25 09:34:06 +0530

    Update About.js

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -3,8 +3,8 @@ import React from 'react'
 export const About = () => {
     return (
         <div>
-            This is an about component 
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis odio, nesciunt sed eveniet, nemo amet, dignissimos magnam itaque ipsam possimus. Inventore vitae, perferendis harum quas cupiditate adipisci repellendus tempore asperiores at consequatur quos?</p>
+            <h2 style="text-align:center">About</h2>
+            <p style="text-align:center">This is a TODO list app in which you can add your work which you want to do and delete them when they are done</p>
         </div>
     )
 }
