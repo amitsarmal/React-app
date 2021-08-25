dc6cd4f15da6e89f8096a837138921b824f6b309
Amit <amit.kr8122001@gmail.com>
2021-08-25 12:24:21 +0530

    Revert "updated about"
    This reverts commit 2f6ab45c0d876b09d8f350f439b6f80793b1c535.

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const About = () => {
     return (
-        <div className="About">
-            <h2>About</h2>
-            <p>This is a TODO list app in which you can add your work which you want to do and delete them when they are done.</p>
+        <div>
+            <h2 style="text-align:center">About</h2>
+            <p style="text-align:center">This is a TODO list app in which you can add your work which you want to do and delete them when they are done</p>
         </div>
     )
 }
